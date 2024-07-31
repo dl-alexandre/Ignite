36329abab5039f45c16fce687700e132771142ca
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-07-31 14:07:09 -0700

    Move Scripts around

--- Sources/Ignite/Elements/Map.swift
+++ Sources/Ignite/Elements/Map.swift
@@ -7,23 +7,35 @@
 
 import Foundation
 
+
+///Pick only the interfaces you need to optimize your app load time. MapKit JS divides its interfaces into libraries that you can specify when loading the framework:
+
 enum MapLibrary {
-    case map, fullmap, annotations, services, overlays, geojson
+    case map, fullmap, annotations, services, overlays, geojson, userlocation
     
     var libraryString: String {
         switch self {
-            case .map: 
+            /// Basic mapkit.Map features without overlays, annotations, and relevant data types.
+            case .map:
                 return "map"
+            /// All mapkit.Map features and relevant data types.
             case .fullmap:
                 return "full-map"
+            /// Annotations, data types, and displays on mapkit.Map.
             case .annotations:
                 return "annotations"
+            /// All services interfaces (such as Search and Geocoder) and relevant data types.
             case .services:
                 return "services"
+            /// Overlays, data types, and displays on mapkit.Map.
             case .overlays:
                 return "overlays"
+            /// The GeoJSON importer.
             case .geojson:
                 return "geojson"
+            /// User location display and controls on mapkit.Map.
+            case .userlocation:
+                return "user-location"
         }
     }
 }
@@ -56,13 +68,18 @@ public struct Map: BlockElement, InlineElement, LazyLoadable {
     /// - Parameter context: The current publishing context.
     /// - Returns: The HTML for this element.
     public func render(context: PublishingContext) -> String {
+        
+        Script(file: "https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js")
+            .addCustomAttribute(name: "crossorigin", value: "anonymous")
+            .addCustomAttribute(name: "async", value: "async")
+            .data("callback", "initMapKit")
+            .data("libraries", "map")
+            .data("token", "eyJraWQiOiJSNzgzRjNIOE05IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyTVA4UVdLN1I2IiwiaWF0IjoxNzIxODU1OTU0LCJleHAiOjE3MjI0OTU1OTl9.ViD505SG9QW54fv5h3iv9lQsY328OlHF-1mEPdFeNzRmqrRI137IaXlGXr2W5lR9brWG7Luej1SoEyr8beXsjw")
+        
         return Group {
-            Script(file: "https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js")
-                .addCustomAttribute(name: "crossorigin", value: "anonymous")
-                .addCustomAttribute(name: "async", value: "async")
-                .data("callback", "initMapKit")
-                .data("libraries", "map")
-                .data("token", "eyJraWQiOiJSNzgzRjNIOE05IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyTVA4UVdLN1I2IiwiaWF0IjoxNzIxODU1OTU0LCJleHAiOjE3MjI0OTU1OTl9.ViD505SG9QW54fv5h3iv9lQsY328OlHF-1mEPdFeNzRmqrRI137IaXlGXr2W5lR9brWG7Luej1SoEyr8beXsjw")
+            
+            
+            
             
             Script(code:
         """
