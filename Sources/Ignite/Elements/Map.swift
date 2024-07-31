5275c265a8abf014cb37d9ecd7af5c6c5e6384c1
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-07-31 15:19:22 -0700

    Include Lat, Long in Map Element

--- Sources/Ignite/Elements/Map.swift
+++ Sources/Ignite/Elements/Map.swift
@@ -57,11 +57,19 @@ public struct Map: BlockElement, InlineElement, LazyLoadable {
     /// Direct, inline JavaScript code to execute.
     private var token: String?
     
+    /// Longitude
+    private var longitude: Double
+    
+    /// Latitude
+    private var latitude: Double
+    
+    
     /// The content to place inside the text.
     
     /// Embeds some custom, inline JavaScript on this page.
-    public init() {
-       
+    public init(longitude: Double, latitude: Double) {
+        self.longitude = longitude
+        self.latitude = latitude
     }
     
     /// Renders this element using publishing context passed in.
@@ -69,17 +77,14 @@ public struct Map: BlockElement, InlineElement, LazyLoadable {
     /// - Returns: The HTML for this element.
     public func render(context: PublishingContext) -> String {
         
+        let loadMapScript = Script(file: "https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js")
+            .addCustomAttribute(name: "crossorigin", value: "anonymous")
+            .addCustomAttribute(name: "async", value: "async")
+            .data("callback", "initMapKit")
+            .data("libraries", "map")
+            .data("token", "eyJraWQiOiJSNzgzRjNIOE05IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyTVA4UVdLN1I2IiwiaWF0IjoxNzIxODU1OTU0LCJleHAiOjE3MjI0OTU1OTl9.ViD505SG9QW54fv5h3iv9lQsY328OlHF-1mEPdFeNzRmqrRI137IaXlGXr2W5lR9brWG7Luej1SoEyr8beXsjw")
         
-        
-        return Group {
-            Script(file: "https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js")
-                .addCustomAttribute(name: "crossorigin", value: "anonymous")
-                .addCustomAttribute(name: "async", value: "async")
-                .data("callback", "initMapKit")
-                .data("libraries", "map")
-                .data("token", "eyJraWQiOiJSNzgzRjNIOE05IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyTVA4UVdLN1I2IiwiaWF0IjoxNzIxODU1OTU0LCJleHAiOjE3MjI0OTU1OTl9.ViD505SG9QW54fv5h3iv9lQsY328OlHF-1mEPdFeNzRmqrRI137IaXlGXr2W5lR9brWG7Luej1SoEyr8beXsjw")
-            
-            Script(code:
+        let mapScript = Script(code:
         """
             // Wait for MapKit JS to be ready to use.
                 const setupMapKitJs = async() => {
@@ -92,26 +97,28 @@ public struct Map: BlockElement, InlineElement, LazyLoadable {
                     }
                 };
                 
-                /**
-                 * Script Entry Point
-                 */
                 const main = async() => {
                     await setupMapKitJs();
                     
                     const cupertino = new mapkit.CoordinateRegion(
-                                                                  new mapkit.Coordinate(37.3316850890998, -122.030067374026),
-                                                                  new mapkit.CoordinateSpan(0.167647972, 0.354985255)
+                        new mapkit.Coordinate(\(longitude), \(latitude)),
+                        new mapkit.CoordinateSpan(0.167647972, 0.354985255)
                                                                   );
                                                                   
-                                                                  // Create a map in the element whose ID is "map-container".
-                                                                  const map = new mapkit.Map("map-container");
-                                                                  map.region = cupertino;
+                // Create a map in the element whose ID is "map-container".
+                    const map = new mapkit.Map("map-container");
+                    map.region = cupertino;
                 };
                 
                 main();
         """
-            )
+        )
             .addCustomAttribute(name: "type", value: "module")
+        
+        return Group {
+            loadMapScript
+            mapScript
+           
             """
                 <style>
                     #map-container {
@@ -125,74 +132,5 @@ public struct Map: BlockElement, InlineElement, LazyLoadable {
         .id("map-container")
         .attributes(attributes)
         .render(context: context)
-        
-//        return output
-//        if let file {
-//            return "<script\(attributes.description) src=\"\(context.site.url.path)\(file)\"></script>"
-//        } else if let code {
-//            return "<script\(attributes.description)>\(code)</script>"
-//        } else {
-//            context.addWarning("""
-//            Creating a script with no source or code should not be possible. \
-//            Please file a bug report on the Ignite project.
-//            """)
-//            return ""
-//        }
     }
 }
-
-
-
-/*
-
- 
- func test_attributes() {
-     let element = Script(file: "/code.js")
-         .data("key", "value")
-         .addCustomAttribute(name: "custom", value: "part")
-     let output = element.render(context: publishingContext)
-
-     XCTAssertEqual(output, "<script custom=\"part\" data-key=\"value\" src=\"/code.js\"></script>")
- }
- 
-<iframe src="https://embed.apple-mapkit.com/v1/place?place=I2FEB0F2FB4D7B0EE&token=eyJraWQiOiJSNzgzRjNIOE05IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyTVA4UVdLN1I2IiwiaWF0IjoxNzIxODU1OTU0LCJleHAiOjE3MjI0OTU1OTl9.ViD505SG9QW54fv5h3iv9lQsY328OlHF-1mEPdFeNzRmqrRI137IaXlGXr2W5lR9brWG7Luej1SoEyr8beXsjw" width="580" height="580" frameborder="0"></iframe>
-
-
-<script src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js"
-    crossorigin async
-    data-callback="initMapKit"
-    data-libraries="map"
-    data-token="IMPORTANT: ADD YOUR TOKEN HERE">
-</script>
-
-<script type="module">
-// Wait for MapKit JS to be ready to use.
-const setupMapKitJs = async() => {
-    // If MapKit JS is not yet loaded...
-    if (!window.mapkit || window.mapkit.loadedLibraries.length === 0) {
-        // ...await <script>'s data-callback (window.initMapKit).
-        await new Promise(resolve => { window.initMapKit = resolve });
-        // Clean up.
-        delete window.initMapKit;
-    }
-};
-
-/**
- * Script Entry Point
- */
-const main = async() => {
-    await setupMapKitJs();
-
-    const cupertino = new mapkit.CoordinateRegion(
-        new mapkit.Coordinate(37.3316850890998, -122.030067374026),
-        new mapkit.CoordinateSpan(0.167647972, 0.354985255)
-    );
-
-    // Create a map in the element whose ID is "map-container".
-    const map = new mapkit.Map("map-container");
-    map.region = cupertino;
-};
-
-main();
- 
-*/
